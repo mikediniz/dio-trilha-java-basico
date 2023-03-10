0ddf6e67210223b1adef05416ff2a34a426027e8
mikediniz <mikediniz.system@gmail.com>
2023-03-10 20:35:29 -0300

    Tipos e variáveis-4

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroCurto;
+
+        int numero = 5;
+
+        final double VALOR_DE_PI = 3.14;
+
+        numero = 10;
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto2);
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
+        
     }
 }
