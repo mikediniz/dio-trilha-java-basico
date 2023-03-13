9578efb83cfa84776557085ad4fd6407848587ec
mikediniz <mikediniz.system@gmail.com>
2023-03-13 03:54:01 -0300

    Boletim

--- java-anatomia-classes/src/edu/mike/primeirasemana/BoletimEstudantil.java
+++ java-anatomia-classes/src/edu/mike/primeirasemana/BoletimEstudantil.java
@@ -5,7 +5,7 @@ public class BoletimEstudantil {
 
     public static void main(String[] args) {
         int mediaFinal = 7;
-        if (mediaFinal < 6)
+        if (mediaFinal > 7)
             System.out.println("REPROVADO");
         else if (mediaFinal == 6)
             System.out.println("PROVA MINERVA");
